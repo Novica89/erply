912a59c8158cc9630f0691782d753bd56eb48aea
Novica89 <vnovica@yahoo.com>
2016-10-19 21:52:59 +0200

    Update erply.php config file
    Fixing bug with using constants instead of referencing .env variables

--- config/erply.php
+++ config/erply.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'user'          => env(ERPLY_USER, 'demo'),
-    'password'      => env(ERPLY_PASS, 'demouser'),
-    'client_code'   => env(ERPLY_CLIENT_CODE, 'eng'),
+    'user'          => env('ERPLY_USER', 'demo'),
+    'password'      => env('ERPLY_PASS', 'demouser'),
+    'client_code'   => env('ERPLY_CLIENT_CODE', 'eng'),
 ];
